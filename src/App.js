923fddddd33cfcb392dfda4269adc3d067d34293
Mikhail <mikhailyarkov2000@gmail.com>
2024-08-12 18:36:55 +0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+const tg = window.Telegra.webApp
 
 function App() {
-  return (
+  useEffect(() => {
+
+  }, [])
+  const handleClose = () => {
+    tg.close()
+  }
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClose}>Закрыть</button>
     </div>
   );
 }
